affaf5134f45db82eb537ca7f9905f0f3e945ba7
anonimatosim1-code <anonimatosim1@gmail.com>
2026-05-23 19:30:51 -0300

    Update 737073905344446575.lua

--- database/users/737073905344446575.lua
+++ database/users/737073905344446575.lua
@@ -2,7 +2,7 @@
 -- ÁREA EDITÁVEL PELO BOT – PLAYLISTS E ARTES
 -- =====================================================
 _G.PLAYLISTS = {
-    "Todas", "Cópias", "Aleatórias", "NSFW", "Memes", "Players"
+    "Todas", "Cópias", "Bundas", "Peitos", "Emojis", "Memes", "Amigos(a", "Players", "Fofoo", "Aleatórias"
 }
 
 _G.ARTS = {
